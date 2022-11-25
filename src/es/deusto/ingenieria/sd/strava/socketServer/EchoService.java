196bcce10b2d76ee91f4e06a8858024433a0628e
Enekourquijo <eneko.urquijo@opendeusto.es>
2022-11-25 13:12:00 +0100

    ultimsa modi
    Co-Authored-By: ismaelalcaide <91288325+ismaelalcaide@users.noreply.github.com>
    Co-Authored-By: JaimeRevilla <90686026+JaimeRevilla@users.noreply.github.com>
    Co-Authored-By: enriquepuente2 <91028770+enriquepuente2@users.noreply.github.com>

--- src/es/deusto/ingenieria/sd/strava/socketServer/EchoService.java
+++ src/es/deusto/ingenieria/sd/strava/socketServer/EchoService.java
@@ -3,6 +3,8 @@ package es.deusto.ingenieria.sd.strava.socketServer;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class process the request of each client as a separated Thread.
@@ -11,6 +13,7 @@ public class EchoService extends Thread {
 	private DataInputStream in;
 	private DataOutputStream out;
 	private Socket tcpSocket;
+	private Map<String, String> mapa;
 
 	public EchoService(Socket socket) {
 		try {
@@ -18,6 +21,8 @@ public class EchoService extends Thread {
 		    this.in = new DataInputStream(socket.getInputStream());
 			this.out = new DataOutputStream(socket.getOutputStream());
 			this.start();
+			mapa = new HashMap<>();
+			mapa.put("1@gmail.com", "12345");
 		} catch (Exception e) {
 			System.out.println("# EchoService - TCPConnection IO error:" + e.getMessage());
 		}
@@ -27,11 +32,28 @@ public class EchoService extends Thread {
 		//Echo server
 		try {
 			//Read request from the client
-			String data = this.in.readUTF();			
+			String data = this.in.readUTF();	
+			String data1 = this.in.readUTF();
 			System.out.println("   - EchoService - Received data from '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data + "'");		
-			
 			//Send response to the client
-			this.out.writeUTF(data.toUpperCase());			
+			if(mapa.containsKey(data)) {
+				if(mapa.get(data).equals(data1)) {
+					data = "true";
+				} else {
+					data = "false";
+				}
+			} else {
+				data = "false";
+			}
+			/*
+			 * Comprobramos el mail y contr en el mapa de mails y contraseñas y 
+			 * si todo da bien devuelve true o false	
+			 * 
+			 * data = true/false
+			 * 
+			 this.out.writeUTF(data);
+			 */
+			this.out.writeUTF(data);
 			System.out.println("   - EchoService - Sent data to '" + tcpSocket.getInetAddress().getHostAddress() + ":" + tcpSocket.getPort() + "' -> '" + data.toUpperCase() + "'");
 		} catch (Exception e) {
 			System.out.println("   # EchoService error" + e.getMessage());
